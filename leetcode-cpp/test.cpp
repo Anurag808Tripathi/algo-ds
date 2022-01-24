1dde761613e38aa87848ecf3b50ba387701c65cf
Anurag808Tripathi <tanurag808@gmail.com>
2022-01-24 13:17:58 +0530

    Intial Commit

---
+++ leetcode-cpp/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+
+
+int main()
+{
+    cout << "Hello world" << endl;
+    int s;
+    s = sum(12, 13);
+    cout << s << endl;
+
+    return 0;
+}
---
+++ leetcode-cpp/twoSum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int arr[] = {2,7,11,15};
+    int target = 9;
+
+    int ans[];
+
+    ans = twoSum(*arr, target);
+    
+    cout<<ans <<end;
+
+
+    return 0;
+}
+
